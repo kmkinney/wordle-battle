94a29191185b81dc9aff4d039de6bdd0266c0115
kmkinney <65429603+kmkinney@users.noreply.github.com>
2022-03-25 19:28:07 -0600

    Added color parsing function

--- components/WordGuess.js
+++ components/WordGuess.js
@@ -64,3 +64,40 @@ function Form(props) {
     </form>
   );
 }
+
+/**
+ * Gets the colors associated with a guess and target word
+ * 
+ * @param {*} target 
+ * @param {*} guess 
+ * @returns 
+ */
+function getColors(target, guess) {
+    let colors = []
+    let tlc = new Map()
+    for(let i = 0; i < target.length; i++){
+        let tl = target[i]
+        if(tlc.has(tl)){
+            tlc.set(tl, tlc.get(tl) + 1)
+        }
+        else{
+            tlc.set(tl, 1)
+        }
+    }
+    for(let i = 0; i < target.length; i++){
+        let tl = target[i]
+        let gl = guess[i]
+        if(tl === gl){
+            colors.push('green')
+            tlc.set(gl, tlc.get(gl) - 1)
+        }
+        else if(tlc.has(gl) && tlc.get(gl) > 0){
+            colors.push('yellow')
+            tlc.set(gl, tlc.get(gl) - 1)
+        }
+        else{
+            colors.push('grey')
+        }
+    }
+    return colors
+}
